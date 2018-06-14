45e6dbbb5f4dd0f741dc06e818e8041cd0487f06
juanesf <juanesf91@gmail.com>
2018-06-14 18:37:21 -0400

    Update easy_install_openwrt.sh
    Fixed

--- BridgeEmulator/easy_install_openwrt.sh
+++ BridgeEmulator/easy_install_openwrt.sh
@@ -7,14 +7,14 @@ wait
 export LC_ALL=C
 mkdir /opt
 mkdir /opt/hue-emulator
-pip3 install requests ws4pi
+pip3 install requests ws4py
 wait
 cd /tmp
 git clone -b OpenWrt git://github.com/juanesf/diyHue.git
 wait
 cd /tmp/diyHue/BridgeEmulator
 cp  HueEmulator3.py config.json updater coap-client-linux cert.pem /opt/hue-emulator/
-cp -r web-ui functions /opt/hue-emulator/
+cp -r web-ui /opt/hue-emulator/
 cp -r functions /opt/hue-emulator/
 wait
 cp hueemulator /etc/init.d/
@@ -29,6 +29,6 @@ chmod +x /opt/hue-emulator/config.json
 /etc/init.d/hueemulator enable
 wait
 
-echo -e "\033[32m Installation completed. run: nano /etc/config/uhttpd and mod htpp port 80 for 82, run: nano /etc/lighttpd/lighttpd.conf and mod  server.port = 80 for 82. For save changes ctrl++x, y, enter..\033[0m"
+echo -e "\033[32m Installation completed. run: nano /etc/config/uhttpd and mod htpp port 80 for 82, run: nano /etc/lighttpd/lighttpd.conf and mod  server.port = 80 for 82. For save changes ctrl +x, y, and enter..\033[0m"
 
 exit 0
