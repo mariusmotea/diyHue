6a5812640819f69ba962ce7c068586ca3f5befc8
juanesf <juanesf91@gmail.com>
2018-06-14 18:09:44 -0400

    update easy_install_openwrt.sh
    Code Optimized...

--- BridgeEmulator/easy_install_openwrt.sh
+++ BridgeEmulator/easy_install_openwrt.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
 opkg update
-sleep 60s
+wait
 opkg install git git-http nano nmap python3 python3-pip
-sleep 60s
+wait
 export LC_ALL=C
 mkdir /opt
 mkdir /opt/hue-emulator
-pip3 install requests
-sleep 120s
+pip3 install requests ws4pi
+wait
 cd /tmp
 git clone -b OpenWrt git://github.com/juanesf/diyHue.git
-sleep 200s
+wait
 cd /tmp/diyHue/BridgeEmulator
 cp  HueEmulator3.py config.json updater coap-client-linux cert.pem /opt/hue-emulator/
 cp -r web-ui functions /opt/hue-emulator/
 cp -r functions /opt/hue-emulator/
-sleep 3s
+wait
 cp hueemulator /etc/init.d/
-sleep 2s
+wait
 chmod +x /etc/init.d/hueemulator
 chmod +x /opt/hue-emulator/HueEmulator3.py
 chmod +x /opt/hue-emulator/coap-client-linux
 chmod +x /opt/hue-emulator/updater
 chmod +x /opt/hue-emulator/web-ui
 chmod +x /opt/hue-emulator/functions
+chmod +x /opt/hue-emulator/config.json
 /etc/init.d/hueemulator enable
-sleep 1s
-echo -e "\033[32m Installation completed. Open Hue app and search for bridges.\033[0m"
+wait
+
+echo -e "\033[32m Installation completed. run: nano /etc/config/uhttpd and mod htpp port 80 for 82, run: nano /etc/lighttpd/lighttpd.conf and mod  server.port = 80 for 82. For save changes ctrl++x, y, enter..\033[0m"
 
 exit 0
