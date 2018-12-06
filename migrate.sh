a5fda6abc92279d01890b95968279efe4407f6b1
shbatm <18057952+shbatm@users.noreply.github.com>
2018-12-06 08:54:57 -0600

    Updated migrate script to include git.
    Fix .gitignore
    
    Fix migrate script error

--- migrate.sh
+++ migrate.sh
@@ -1,6 +1,24 @@
+#!/bin/bash
+
+OLDPWD=$(pwd);
+
 echo -e "\033[36m Migrating Existing diyHue Install\033[0m"
 cd /opt/hue-emulator
 OLD="http://raw.githubusercontent.com/mariusmotea/diyHue/master/BridgeEmulator/updater"
 NEW="http://raw.githubusercontent.com/diyhue/diyHue/master/BridgeEmulator/updater"
 sed -i "s#$OLD#$NEW#g" "HueEmulator3.py"
 echo -e "\033[36m Existing Install Migrated Successfully\033[0m"
+
+echo -e "\033[36m Checking if git was used."
+
+cd $OLDPWD;
+if [[ -d "./.git" ]]; then
+    echo -e "\033[36m Git Found, Updating\033[0m"
+    git remote set-url origin https://github.com/diyhue/diyHue.git
+    rm -rf ./Lights/
+    git clone https://github.com/diyhue/Lights.git
+    rm -rf ./Sensors/
+    git clone https://github.com/diyhue/Devices.git
+    cd $OLDPWD;
+fi
+
