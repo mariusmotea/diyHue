decfa6e487481c430be79c6427037f263b65728f
cheesemarathon <07bridgesb@gmail.com>
2018-12-06 10:03:54 +0000

    Fix user messages

--- migrate.sh
+++ migrate.sh
@@ -1,6 +1,6 @@
-echo "Migrating Existing diyHue Install"
+echo -e "\033[36m Migrating Existing diyHue Install\033[0m"
 cd /opt/hue-emulator
 OLD="http://raw.githubusercontent.com/mariusmotea/diyHue/master/BridgeEmulator/updater"
 NEW="http://raw.githubusercontent.com/diyhue/diyHue/master/BridgeEmulator/updater"
 sed -i "s#$OLD#$NEW#g" "HueEmulator3.py"
-echo "Existing Install Migrated Successfully"
+echo -e "\033[36m Existing Install Migrated Successfully\033[0m"
