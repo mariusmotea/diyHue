62490c8e33d487ca604a4745a49e0dbcffcd925d
cheesemarathon <07bridgesb@gmail.com>
2018-12-17 16:07:38 +0000

    Remove unnecessary CD commands

--- migrate.sh
+++ migrate.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-OLDPWD=$(pwd);
-
 echo -e "\033[36m Migrating Existing diyHue Install\033[0m"
-cd /opt/hue-emulator
+
 OLD="http://raw.githubusercontent.com/mariusmotea/diyHue/master/BridgeEmulator/updater"
 NEW="http://raw.githubusercontent.com/diyhue/diyHue/master/BridgeEmulator/updater"
 sed -i "s#$OLD#$NEW#g" "HueEmulator3.py"
@@ -11,7 +9,6 @@ echo -e "\033[36m Existing Install Migrated Successfully\033[0m"
 
 echo -e "\033[36m Checking if git was used."
 
-cd $OLDPWD;
 if [[ -d "./.git" ]]; then
     echo -e "\033[36m Git Found, Updating\033[0m"
     git remote set-url origin https://github.com/diyhue/diyHue.git
@@ -19,6 +16,4 @@ if [[ -d "./.git" ]]; then
     git clone https://github.com/diyhue/Lights.git
     rm -rf ./Sensors/
     git clone https://github.com/diyhue/Devices.git
-    cd $OLDPWD;
 fi
-
